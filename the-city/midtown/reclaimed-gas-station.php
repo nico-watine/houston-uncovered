eedd13bc1ead1a9a82a9b0536425847cbda1dbe6
Nico Watine <nico@studioncreations.com>
2022-04-16 00:02:18 -0500

    Add site files

---
+++ the-city/midtown/reclaimed-gas-station.php
@@ -0,0 +1,74 @@
+<?php 
+	include($_SERVER['DOCUMENT_ROOT'].'/php/autoVer.php');
+?>
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Reclaimed Gas Station | Houston Uncovered</title>
+	<meta name="description" content="Your daily dose of the city, serving up photos of all that is Houston" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link type="text/plain" rel="author" href="/humans.txt">
+	
+	<link rel="stylesheet" href="<?php autoVer('/css/style.css'); ?>" />
+	<script src="<?php autoVer('/js/picturefill-min.js'); ?>" async></script>
+</head>
+
+<body id="reclaimed-gas-station">
+
+<header id="white" class="shrinking">
+	<div class="outer-container">
+		<div class="row">
+			<div class="logo-wrapper">
+				<a href="/">
+				<div id="sticky-logo">
+					<img src="/img/assets/logo/light/logo@2x.png" alt="Houston Uncovered Logo">
+				</div>
+				<img id="default-logo" src="/img/assets/logo/light/logo@2x.png" alt="Houston Uncovered Logo">
+				</a>
+			</div>
+			<nav>
+				<ul id="main-nav">
+	<li><a href="/">Home</a></li>
+	<li><a href="/the-city/washington-ave/">Washington Ave</a></li>
+	<li><a href="/the-city/midtown/">Midtown</a></li>
+	<li><a href="/the-city/east-end/" id="last">East End</a></li>
+</ul>
+			</nav>
+		</div>
+	</div>
+</header>
+
+<section class="hero">
+	<div class="hero-text light">
+		<h1>Reclaimed Gas Station</h1>
+		<a href="/the-city/midtown/" class="category-link">Midtown</a>
+	</div>
+	<img src="/img/sets/reclaimed-gas-station/hero/reclaimed-gas-station.jpg" alt="Reclaimed Gas Station in Midtown">
+</section>
+
+<main class="gallery">
+		<div class="outer-container">
+		<img id="img-1" class="full-width" srcset="/img/sets/reclaimed-gas-station/gallery/reclaimed-gas-station(2).jpg 1x, /img/sets/reclaimed-gas-station/gallery/reclaimed-gas-station(2)@2x.jpg 2x" alt="Reclaimed Gas Station in Midtown">
+		<img id="img-2" class="full-width" srcset="/img/sets/reclaimed-gas-station/gallery/reclaimed-gas-station(3).jpg 1x, /img/sets/reclaimed-gas-station/gallery/reclaimed-gas-station(3)@2x.jpg 2x" alt="Reclaimed Gas Station in Midtown">
+		<img id="img-3" class="full-width" srcset="/img/sets/reclaimed-gas-station/gallery/reclaimed-gas-station(1).jpg 1x, /img/sets/reclaimed-gas-station/gallery/reclaimed-gas-station(1)@2x.jpg 2x" alt="Reclaimed Gas Station in Midtown">
+	</div>
+</main>
+
+
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="/js/vendor/jquery/1-11-3/jquery-min.js"><\/script>')</script>
+<script src="<?php autoVer('/js/scripts-min.js'); ?>"></script>
+
+<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+	function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+	e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+	e.src='https://www.google-analytics.com/analytics.js';
+	r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-55459011-5','auto');ga('send','pageview');
+</script>
+
+</body>
+</html>
